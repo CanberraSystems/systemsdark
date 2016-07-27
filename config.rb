82d234cecaabc6b31104b6d96fec927889341c20
CanberraSystems <canberra.systems@gmail.com>
2016-07-27 20:38:53 +1000

    File uploads
    Drupal sub-theme and SASS files

---
+++ config.rb
@@ -0,0 +1,23 @@
+require 'compass/import-once/activate'
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+
+
+http_path = "/"
+css_dir = "stylesheets"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "javascripts"
+fonts_dir = "fonts"
+
+output_style = :nested
+sourcemap = false
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+relative_assets = false
+sass_options = {:debug_info=>false}
+
+line_comments = false
+
+preferred_syntax = :scss
